607dbeace1512a9cdcc828320b2bca4cbd61bae2
Tzu-ping Chung <uranusjr@gmail.com>
2020-01-29 14:46:22 +0800

    Include editable locations in JSON output of list
    This matches the JSON format with the default columns format.

--- src/pip/_internal/commands/list.py
+++ src/pip/_internal/commands/list.py
@@ -303,8 +303,9 @@ def format_for_json(packages, options):
             'name': dist.project_name,
             'version': six.text_type(dist.version),
         }
-        if options.verbose >= 1:
+        if options.verbose >= 1 or dist_is_editable(dist):
             info['location'] = dist.location
+        if options.verbose >= 1:
             info['installer'] = get_installer(dist)
         if options.outdated:
             info['latest_version'] = six.text_type(dist.latest_version)
